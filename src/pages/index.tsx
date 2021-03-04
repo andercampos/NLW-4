9947b7ea27532714060cadb1e09a67f6b36e4f2a
Anderson Campos <anderson.campos@pipefy.com>
2021-03-04 01:25:05 -0300

    Add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 import CompletedChallenges from '../components/CompletedChallenges';
+// import Countdown from '../components/Countdown';
+
+import Head from 'next/head';
 
 import { Container } from '../styles/pages/Home';
 
 const Home: React.FC = () => {
   return (
     <Container>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          {/* <Countdown /> */}
         </div>
         <div>
         </div>
